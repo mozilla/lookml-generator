afb54f364d30c4ff3bfc2d3ecdbda51f2fa44c9e
Daniel Thorn <dthorn@mozilla.com>
2021-03-08 11:02:50 -0800

    address review

--- generator/namespaces.py
+++ generator/namespaces.py
@@ -1,14 +1,15 @@
 """Generate namespaces.yaml."""
+import json
 import tarfile
-import urllib
+import urllib.request
 import warnings
+from collections import defaultdict
 from io import BytesIO
 from itertools import groupby
 from operator import itemgetter
 from pathlib import Path
 
 import click
-import requests
 import yaml
 
 
@@ -19,14 +20,12 @@ def _get_first(tuple_):
 def _get_views(uri):
     with urllib.request.urlopen(uri) as f:
         tarbytes = BytesIO(f.read())
-    views = {}
+    views = defaultdict(dict)
     with tarfile.open(fileobj=tarbytes, mode="r:gz") as tar:
         for tarinfo in tar:
             if tarinfo.name.endswith("/view.sql"):
                 *_, project, dataset_id, view_id, _ = tarinfo.name.split("/")
                 if project == "moz-fx-data-shared-prod":
-                    if dataset_id not in views:
-                        views[dataset_id] = {}
                     views[dataset_id][view_id] = tar.extractfile(tarinfo.name)
     return views
 
@@ -44,45 +43,44 @@ def _get_views(uri):
     help="URI of a tar archive of the bigquery-etl generated-sql branch, which is "
     "used to list views and determine whether they reference stable tables",
 )
-def namespaces(custom_namespaces, generated_sql_uri):
+@click.option(
+    "--app-listings-uri",
+    default="https://probeinfo.telemetry.mozilla.org/v2/glean/app-listings",
+    help="URI for probeinfo service v2 glean app listings",
+)
+def namespaces(custom_namespaces, generated_sql_uri, app_listings_uri):
     """Generate namespaces.yaml."""
     warnings.filterwarnings("ignore", module="google.auth._default")
 
+    # define key function and reuse it for sorted and groupby
     get_app_name = itemgetter("app_name")
-    app_listings = sorted(
-        requests.get(
-            "https://probeinfo.telemetry.mozilla.org/v2/glean/app-listings"
-        ).json(),
-        key=get_app_name,
-    )
-    app_filters = {"app_channel": "channel", "app_id": "app_id"}
+    # groupby requires input be sorted by key to produce one result per key
+    with urllib.request.urlopen(app_listings_uri) as f:
+        app_listings = sorted(json.load(f), key=get_app_name)
     view_definitions = _get_views(generated_sql_uri)
     namespaces = {}
     for app_name, group in groupby(app_listings, get_app_name):
-        apps = list(group)
-        views = {}
+        views = defaultdict(list)
         canonical_app_name = None
-        for app in apps:
+        for app in group:
             if canonical_app_name is None or app.get("app_channel") == "release":
                 canonical_app_name = app["canonical_app_name"]
             dataset_id = app["bq_dataset_family"]
             for view_id in view_definitions[dataset_id]:
                 if view_id == "deletion_request":
                     continue
-                if view_id not in views:
-                    views[view_id] = []
                 table = {"table": f"mozdata.{dataset_id}.{view_id}"}
-                for attr, column in app_filters.items():
+                for attr, column in [("app_channel", "channel"), ("app_id", "app_id")]:
                     if attr in app:
                         table[column] = app[attr]
                 views[view_id].append(table)
 
         namespaces[app_name] = {
             "canonical_app_name": canonical_app_name,
-            "views": views,
+            "views": dict(views),
         }
 
     if custom_namespaces is not None:
         namespaces.update(yaml.safe_load(custom_namespaces.read()))
 
-    (Path(__file__).parent.parent / "namespaces.yaml").write_text(yaml.dump(namespaces))
+    Path("namespaces.yaml").write_text(yaml.safe_dump(namespaces))
