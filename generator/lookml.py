5a702a7d385ab155e8c4a52c6ea8df84a292d0fd
Daniel Thorn <dthorn@mozilla.com>
2021-03-23 09:27:28 -0700

    Generate views via lkml (#26)

--- generator/lookml.py
+++ generator/lookml.py
@@ -1,12 +1,9 @@
 """Generate lookml from namespaces."""
-import json
 import logging
 import re
-from dataclasses import dataclass
-from functools import cached_property
+from itertools import filterfalse
 from pathlib import Path
-from textwrap import indent
-from typing import Dict, Iterable, List, Optional, Tuple
+from typing import Any, Dict, Iterable, List, Tuple
 
 import click
 import lkml
@@ -41,196 +38,145 @@ MAP_LAYER_NAMES = {
 }
 
 
-@dataclass(frozen=True)
-class Dimension:
-    """LookML View Dimension."""
+def _get_dimension(path: Tuple[str, ...], field_type: str, mode: str) -> Dict[str, Any]:
+    result: Dict[str, Any] = {}
+    result["sql"] = "${TABLE}." + ".".join(path)
+    name = path
+    if mode == "REPEATED" or path in HIDDEN_DIMENSIONS:
+        result["hidden"] = "yes"
+    else:
+        result["type"] = BIGQUERY_TYPE_TO_DIMENSION_TYPE[field_type]
+        if result["type"] == "time":
+            # Remove _{type} suffix from the last path element for dimension group
+            # names For example submission_date and submission_timestamp become
+            # submission, and metadata.header.parsed_date becomes
+            # metadata__header__parsed. This is because the timeframe will add a _{type}
+            # suffix to the individual dimension names.
+            name = *path[:-1], re.sub("_(date|time(stamp)?)$", "", path[-1])
+            result["timeframes"] = [
+                "raw",
+                "time",
+                "date",
+                "week",
+                "month",
+                "quarter",
+                "year",
+            ]
+            if field_type == "DATE":
+                result["timeframes"].remove("time")
+                result["convert_tz"] = "no"
+                result["datatype"] = "date"
+        if len(path) > 1:
+            result["group_label"] = " ".join(path[:-1]).replace("_", " ").title()
+            result["group_item_label"] = path[-1].replace("_", " ").title()
+        if path in MAP_LAYER_NAMES:
+            result["map_layer_name"] = MAP_LAYER_NAMES[path]
+    result["name"] = "__".join(name)
+    return result
 
-    path: Tuple[str, ...]
-    field_type: str
-    mode: str
 
-    @cached_property
-    def hidden(self) -> bool:
-        """Determine whether this dimension is hidden."""
-        return self.mode == "REPEATED" or self.path in HIDDEN_DIMENSIONS
-
-    @cached_property
-    def type(self) -> str:
-        """Determine the lookml type for this dimension."""
-        return BIGQUERY_TYPE_TO_DIMENSION_TYPE[self.field_type]
-
-    @cached_property
-    def name(self) -> str:
-        """Determine the dimension or dimension_group name.
-
-        Remove _{type} suffix from the last path element for time dimension_group names.
-        For example submission_date and submission_timestamp become submission, and
-        metadata.header.parsed_date becomes metadata__header__parsed. This is because
-        the timeframe will add a {type} suffix to the individual dimension names.
-        """
-        if not self.hidden and self.type == "time":
-            path = *self.path[:-1], re.sub("_(date|time(stamp)?)$", "", self.path[-1])
+def _generate_dimensions_helper(
+    schema: List[bigquery.SchemaField], *prefix: str
+) -> Iterable[dict]:
+    for field in sorted(schema, key=lambda f: f.name):
+        if field.field_type == "RECORD" and not field.mode == "REPEATED":
+            yield from _generate_dimensions_helper(field.fields, *prefix, field.name)
         else:
-            path = self.path
-        return "__".join(path)
+            yield _get_dimension((*prefix, field.name), field.field_type, field.mode)
 
-    def to_lookml(self) -> str:
-        """Serialize dimension as lookml."""
-        key = "dimension"
-        attributes: Dict[str, str] = {}
-        attributes["sql"] = "${TABLE}." + ".".join(self.path) + " ;;"
-        if self.hidden:
-            attributes["hidden"] = "yes"
-        else:
-            attributes["type"] = self.type
-            if self.type == "time":
-                key = "dimension_group"
-                timeframes = [
-                    "raw",
-                    "time",
-                    "date",
-                    "week",
-                    "month",
-                    "quarter",
-                    "year",
-                ]
-                if self.field_type == "DATE":
-                    timeframes.remove("time")
-                    attributes["convert_tz"] = "no"
-                    attributes["datatype"] = "date"
-                attributes["timeframes"] = (
-                    "[\n" + indent(",\n".join(timeframes), " " * 2) + "\n]"
-                )
-            if len(self.path) > 1:
-                attributes["group_label"] = json.dumps(
-                    " ".join(self.path[:-1]).replace("_", " ").title()
-                )
-                attributes["group_item_label"] = json.dumps(
-                    self.path[-1].replace("_", " ").title()
-                )
-            if self.path in MAP_LAYER_NAMES:
-                attributes["map_layer_name"] = MAP_LAYER_NAMES[self.path]
-        return (
-            f"{key}: {self.name} {{"
-            + indent(
-                "".join(
-                    f"\n{attr}: {value}" for attr, value in sorted(attributes.items())
-                ),
-                " " * 2,
+
+def _generate_dimensions(client: bigquery.Client, table: str) -> List[Dict[str, Any]]:
+    """Generate dimensions and dimension groups from a bigquery table.
+
+    When schema contains both submission_timestamp and submission_date, only produce
+    a dimension group for submission_timestamp.
+
+    Raise ClickException if schema results in duplicate dimensions.
+    """
+    dimensions = {}
+    for dimension in _generate_dimensions_helper(client.get_table(table).schema):
+        name = dimension["name"]
+        # overwrite duplicate "submission" dimension group, thus picking the
+        # last value sorted by field name, which is submission_timestamp
+        if name in dimensions and name != "submission":
+            raise click.ClickException(
+                f"duplicate dimension {name!r} for table {table!r}"
             )
-            + "\n}"
-        )
-
-    @classmethod
-    def from_schema(
-        klass, schema: List[bigquery.SchemaField], *prefix: str
-    ) -> List["Dimension"]:
-        """Generate dimensions from a bigquery schema.
-
-        When schema contains both submission_timestamp and submission_date, only produce
-        a dimension group for submission_timestamp.
-
-        Raise KeyError if schema results in duplicate dimensions.
-        """
-        dimensions = {}
-        for field in sorted(schema, key=lambda f: f.name):
-            if field.field_type == "RECORD" and not field.mode == "REPEATED":
-                new_dimensions = klass.from_schema(field.fields, *prefix, field.name)
-            else:
-                path = *prefix, field.name
-                new_dimensions = [klass(path, field.field_type, field.mode)]
-            for dimension in new_dimensions:
-                # overwrite duplicate "submission" dimension_group, thus picking the
-                # last value sorted by field name, which is submission_timestamp
-                if dimension.name in dimensions and dimension.name != "submission":
-                    raise KeyError(dimension.name)
-                dimensions[dimension.name] = dimension
-        return list(dimensions.values())
+        dimensions[name] = dimension
+    return list(dimensions.values())
 
 
-@dataclass
-class Measure:
-    """LookML View Measure."""
+def _is_dimension_group(dimension: dict):
+    """Determine if a dimension is actually a dimension group."""
+    return "timeframes" in dimension or "intervals" in dimension
 
-    name: str
-    type: str
-    sql: Optional[str] = None
 
-    def to_lookml(self) -> str:
-        """Serialize measure as lookml."""
-        return (
-            f"measure: {self.name} {{"
-            + f"\n  type: {self.type}"
-            + (f"\n  sql: {self.sql} ;;" if self.sql is not None else "")
-            + "\n}"
-        )
+def _generate_measures(dimensions: List[dict], table: str) -> List[Dict[str, str]]:
+    """Generate measures from a list of dimensions.
 
-    @classmethod
-    def from_dimensions(klass, dimensions: List[Dimension]) -> List["Measure"]:
-        """Generate measures from a list of dimensions.
+    When no dimension-specific measures are found, return a single "count" measure.
 
-        When no dimension-specific measures are found, return a single "count" measure.
-
-        Raise KeyError if dimensions result in duplicate measures.
-        """
-        measures = {}
-        for dimension in dimensions:
-            if dimension.name in {"client_id", "client_info__client_id"}:
-                measure = klass("clients", "count_distinct", f"${{{dimension.name}}}")
-            elif dimension.name == "document_id":
-                measure = klass("ping_count", "count")
-            else:
-                continue
-            if measure.name in measures:
-                raise KeyError(measure.name)
-            measures[measure.name] = measure
-        # return a generic count measure if no default measures were generated
-        return list(measures.values()) or [klass("count", "count")]
+    Raise ClickException if dimensions result in duplicate measures.
+    """
+    measures = {}
+    for dimension in dimensions:
+        dimension_name = dimension["name"]
+        if dimension_name in {"client_id", "client_info__client_id"}:
+            measure = {
+                "name": "clients",
+                "type": "count_distinct",
+                "sql": f"${{{dimension_name}}}",
+            }
+        elif dimension_name == "document_id":
+            measure = {"name": "ping_count", "type": "count"}
+        else:
+            continue
+        name = measure["name"]
+        if name in measures:
+            raise click.ClickException(
+                f"duplicate measure {name!r} for table {table!r}"
+            )
+        measures[name] = measure
+    # return a generic count measure if no default measures were generated
+    return list(measures.values()) or [{"name": "count", "type": "count"}]
 
 
 def _generate_views(
     client, out_dir: Path, views: Dict[str, List[Dict[str, str]]]
 ) -> Iterable[Path]:
-    for view, tables in views.items():
+    for name, tables in views.items():
+        view: Dict[str, Any] = {"name": name}
         # use schema for the table where channel=="release" or the first one
         table = next(
             (table for table in tables if table.get("channel") == "release"),
             tables[0],
         )["table"]
-        try:
-            dimensions = Dimension.from_schema(client.get_table(table).schema)
-        except KeyError as e:
-            raise click.ClickException(f"duplicate dimension {e} for table {table!r}")
-        try:
-            measures = Measure.from_dimensions(dimensions)
-        except KeyError as e:
-            raise click.ClickException(f"duplicate measure {e} for table {table!r}")
-        fields = []
-        sql_table_name = table
+        # add dimensions and dimension groups
+        dimensions = _generate_dimensions(client, table)
+        view["dimensions"] = list(filterfalse(_is_dimension_group, dimensions))
+        view["dimension_groups"] = list(filter(_is_dimension_group, dimensions))
+        # add measures
+        view["measures"] = _generate_measures(dimensions, table)
         if len(tables) > 1:
             # parameterize table name
-            fields.append(
-                "\n  parameter: channel {"
-                "\n    type: unquoted"
-                + "".join(
-                    "\n    allowed_value: {"
-                    f'\n      label: {json.dumps(table["channel"].title())}'
-                    f'\n      value: {json.dumps(table["table"])}'
-                    "\n    }"
-                    for table in tables
-                )
-                + "\n  }"
-            )
-            sql_table_name = "{% parameter channel %}"
-        fields.append(f"\n  sql_table_name: `{sql_table_name}` ;;")
-        # add dimensions and measures after sql_table_name
-        fields += sorted(
-            indent(f"\n{field.to_lookml()}", " " * 2)
-            for field in dimensions + measures  # type: ignore
-        )
-        path = out_dir / (view + ".view.lkml")
-        path.write_text(f"view: {view} {{" + "\n".join(fields) + "\n}")
-
+            view["parameters"] = [
+                {
+                    "name": "channel",
+                    "type": "unquoted",
+                    "allowed_values": [
+                        {
+                            "label": table["channel"].title(),
+                            "value": table["table"],
+                        }
+                        for table in tables
+                    ],
+                }
+            ]
+            view["sql_table_name"] = "`{% parameter channel %}`"
+        else:
+            view["sql_table_name"] = f"`{table}`"
+        path = out_dir / f"{name}.view.lkml"
+        path.write_text(lkml.dump({"views": [view]}))
         yield path
 
 
