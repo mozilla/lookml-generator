2788b341709f309a361251ec7c9cdaa02f0868f5
Frank Bertsch <fbertsch@mozilla.com>
2021-04-12 17:16:05 -0400

    Return instances of views instead of dicts
    - Create more functions in namespaces.py
    - Add more unit tests

--- generator/explores.py
+++ generator/explores.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Dict, Iterator, List
 
+from .views import PingView, View
+
 
 @dataclass
 class Explore:
@@ -16,6 +18,15 @@ class Explore:
     def to_dict(self) -> dict:
         """Explore instance represented as a dict."""
         return {self.name: {"type": self.type, "views": self.views}}
+
+    def to_lookml(self) -> dict:
+        """Generate LookML for this explore."""
+        raise NotImplementedError("Only implemented in subclasses")
+
+    @staticmethod
+    def from_dict(name: str, defn: dict) -> Explore:
+        """Get an instance of an explore from a namespace definition."""
+        raise NotImplementedError("Only implemented in subclasses")
 
 
 @dataclass
@@ -30,12 +41,11 @@ class PingExplore(Explore):
         }
 
     @staticmethod
-    def from_views(views: Dict[str, List[Dict[str, str]]]) -> Iterator[PingExplore]:
+    def from_views(views: List[View]) -> Iterator[PingExplore]:
         """Generate all possible PingExplores from the views."""
-        for view, channel_infos in views.items():
-            is_ping_tbl = all((c.get("is_ping_table", False) for c in channel_infos))
-            if is_ping_tbl:
-                yield PingExplore(view, "ping_explore", {"base_view": view})
+        for view in views:
+            if view.view_type == PingView.type:
+                yield PingExplore(view.name, "ping_explore", {"base_view": view.name})
 
     @staticmethod
     def from_dict(name: str, defn: dict) -> PingExplore:
@@ -47,18 +57,19 @@ class GrowthAccountingExplore(Explore):
     """A Growth Accounting Explore, from Baseline Clients Last Seen."""
 
     @staticmethod
-    def from_views(views: Dict[str, List[Dict[str, str]]]) -> Iterator[PingExplore]:
+    def from_views(views: List[View]) -> Iterator[GrowthAccountingExplore]:
         """
         If possible, generate a Growth Accounting explore for this namespace.
 
         Growth accounting explores are only created for growth_accounting views.
         """
-        if "growth_accounting" in views.keys():
-            yield GrowthAccountingExplore(
-                "growth_accounting",
-                "growth_accounting_explore",
-                {"base_view": "growth_accounting"},
-            )
+        for view in views:
+            if view.name == "growth_accounting":
+                yield GrowthAccountingExplore(
+                    "growth_accounting",
+                    "growth_accounting_explore",
+                    {"base_view": "growth_accounting"},
+                )
 
 
 explore_types = {
