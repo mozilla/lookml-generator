d6cd5b59a51ffd751faeee6781f50d795f3ab3bf
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-04-30 12:25:06 -0700

    Refactor client id field and add sum/client counts to measures

--- generator/views/glean_ping_view.py
+++ generator/views/glean_ping_view.py
@@ -1,5 +1,8 @@
 """Class to describe a Glean Ping View."""
+from collections import Counter
 from typing import Any, Dict, List
+
+import click
 
 from .ping_view import PingView
 
@@ -35,3 +38,46 @@ class GleanPingView(PingView):
             self._annotate_dimension(d)
             for d in super().get_dimensions(bq_client, table)
         ]
+
+    def get_measures(self, dimensions: List[dict], table: str) -> List[Dict[str, str]]:
+        """Generate measures from a list of dimensions.
+
+        When no dimension-specific measures are found, return a single "count" measure.
+
+        Raise ClickException if dimensions result in duplicate measures.
+        """
+        measures = super().get_measures(dimensions, table)
+        client_id_field = self._get_client_id(dimensions, table)
+
+        for dimension in dimensions:
+            dimension_name = dimension["name"]
+            if "metrics__counter__" in dimension_name:
+                # handle the counters in the metric ping
+                name = dimension_name.ltrim("metrics__")
+                measures += [
+                    {
+                        "name": name,
+                        "type": "sum",
+                        "sql": f"${{{dimension_name}}}",
+                    },
+                    {
+                        "name": f"{name}_client_count",
+                        "type": "count_distinct",
+                        "sql": (
+                            f"case when ${{{dimension_name}}} > 0 then "
+                            f"${{{client_id_field}}}"
+                        ),
+                    },
+                ]
+
+        # check if there are any duplicate values, and report the first one that
+        # shows up
+        names = [measure["name"] for measure in measures]
+        duplicates = [k for k, v in Counter(names).items() if v > 1]
+        if duplicates:
+            name = duplicates[0]
+            raise click.ClickException(
+                f"duplicate measure {name!r} for table {table!r}"
+            )
+
+        return measures
