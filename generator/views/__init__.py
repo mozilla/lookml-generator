8f433fa4bdf571d9813f28b64a3cc5c4d8d55d00
Frank Bertsch <fbertsch@mozilla.com>
2021-05-03 15:30:49 -0400

    Add TableView to tables
    A TableView simply exposes a raw table, with no measures.
    
    Eventually we will import this instead of re-generating
    all dimensions multiple times.

--- generator/views/__init__.py
+++ generator/views/__init__.py
@@ -1,9 +1,11 @@
 """All available Looker views."""
 from .growth_accounting_view import GrowthAccountingView
 from .ping_view import PingView
+from .table_view import TableView
 from .view import View, ViewDict  # noqa: F401
 
 VIEW_TYPES = {
     PingView.type: PingView,
     GrowthAccountingView.type: GrowthAccountingView,
+    TableView.type: TableView,
 }
