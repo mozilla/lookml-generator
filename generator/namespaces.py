b35dc9d11a2435a671cbe2efd37a2ff0d49e2c87
Anna Scholtz <anna@scholtzan.net>
2022-04-21 11:53:10 -0700

    Fix parsing of opmon titles (#421)

--- generator/namespaces.py
+++ generator/namespaces.py
@@ -94,7 +94,9 @@ def _get_opmon(bq_client: bigquery.Client, namespaces: Dict[str, Any]):
     # Iterating over all defined operational monitoring projects
     for project in projects:
         table_prefix = _normalize_slug(project["slug"])
-        project_name = "_".join(project["name"].lower().replace(",", "").split(" "))
+        project_name = re.sub(
+            "[^0-9a-zA-Z_]+", "_", "_".join(project["name"].lower().split(" "))
+        )
         branches = project.get("branches", ["enabled", "disabled"])
 
         for data_type in DATA_TYPES:
