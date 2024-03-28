e27c74eeeb12415d55dae9f1319fa25f2be094dd
Anna Scholtz <anna@scholtzan.net>
2024-03-28 10:47:07 -0700

    Remove namespaces from spoke that no longer exist

--- generator/spoke.py
+++ generator/spoke.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import shutil
 from pathlib import Path
 from typing import Dict, List, TypedDict
 
@@ -148,8 +149,14 @@ def generate_directories(
     namespaces: Dict[str, NamespaceDict], base_dir: Path, sdk_setup=False
 ):
     """Generate directories and model for a namespace, if it doesn't exist."""
+    seen_spoke_namespaces = {}
     for namespace, defn in namespaces.items():
         spoke = defn["spoke"]
+        if spoke not in seen_spoke_namespaces:
+            seen_spoke_namespaces[spoke] = [namespace]
+        else:
+            seen_spoke_namespaces[spoke].append(namespace)
+
         spoke_dir = base_dir / spoke
         spoke_dir.mkdir(parents=True, exist_ok=True)
         print(f"Writing {namespace} to {spoke_dir}")
@@ -171,6 +178,18 @@ def generate_directories(
             sdk = looker_sdk.init40()
             logging.info("Looker SDK 4.0 initialized successfully.")
             configure_model(sdk, namespace, db_connection, spoke_project)
+
+    # remove directories for namespaces that got removed
+    for spoke in seen_spoke_namespaces.keys():
+        spoke_dir = base_dir / spoke
+        existing_dirs = {p.name for p in spoke_dir.iterdir()}
+
+        for existing_dir in existing_dirs:
+            # make sure the directory belongs to a namespace by checking if a model file exists
+            if Path(existing_dir / f"{existing_dir}.model.lkml").is_file():
+                if existing_dir not in seen_spoke_namespaces[spoke]:
+                    # namespace does not exists anymore, remove directory
+                    shutil.rmtree(Path(existing_dir))
 
 
 @click.command(help=__doc__)
