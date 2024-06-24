09c4aa9f8e9bb06640991b13aa69d75bfca325e8
Anna Scholtz <anna@scholtzan.net>
2024-06-24 08:10:33 -0700

    Parallel lookml generation (#994)
    * Parallel generation
    
    * Run lookml geneation in parallel
    
    * Parallelism parameter
    
    * Use starmap

--- generator/lookml.py
+++ generator/lookml.py
@@ -1,8 +1,10 @@
 """Generate lookml from namespaces."""
 
 import logging
+from functools import partial
+from multiprocessing.pool import ThreadPool
 from pathlib import Path
-from typing import Dict, Iterable, Optional
+from typing import Any, Dict, Iterable, Optional
 
 import click
 import lkml
@@ -25,68 +27,67 @@ FILE_HEADER = """
 """
 
 
-def _generate_views(
-    client, out_dir: Path, views: Iterable[View], v1_name: Optional[str]
-) -> Iterable[Path]:
-    for view in views:
-        logging.info(
-            f"Generating lookml for view {view.name} in {view.namespace} of type {view.view_type}"
-        )
-        path = out_dir / f"{view.name}.view.lkml"
-        lookml = view.to_lookml(client, v1_name)
-        if lookml == {}:
-            continue
+def _generate_view(
+    client, out_dir: Path, view: View, v1_name: Optional[str]
+) -> Optional[Path]:
+    logging.info(
+        f"Generating lookml for view {view.name} in {view.namespace} of type {view.view_type}"
+    )
+    path = out_dir / f"{view.name}.view.lkml"
+    lookml = view.to_lookml(client, v1_name)
+    if lookml == {}:
+        return None
 
-        # lkml.dump may return None, in which case write an empty file
-        path.write_text(FILE_HEADER + (lkml.dump(lookml) or ""))
-        yield path
+    # lkml.dump may return None, in which case write an empty file
+    path.write_text(FILE_HEADER + (lkml.dump(lookml) or ""))
+    return path
 
 
-def _generate_explores(
+def _generate_explore(
     client,
     out_dir: Path,
     namespace: str,
-    explores: dict,
+    explore_name: str,
+    explore: Any,
     views_dir: Path,
     v1_name: Optional[
         str
     ],  # v1_name for Glean explores: see: https://mozilla.github.io/probe-scraper/#tag/library
-) -> Iterable[Path]:
-    for explore_name, defn in explores.items():
-        logging.info(f"Generating lookml for explore {explore_name} in {namespace}")
-        explore = EXPLORE_TYPES[defn["type"]].from_dict(explore_name, defn, views_dir)
-        file_lookml = {
-            # Looker validates all included files,
-            # so if we're not explicit about files here, validation takes
-            # forever as looker re-validates all views for every explore (if we used *).
-            "includes": [
-                f"/looker-hub/{namespace}/views/{view}.view.lkml"
-                for view in explore.get_dependent_views()
-            ],
-            "explores": explore.to_lookml(client, v1_name),
-        }
-        path = out_dir / (explore_name + ".explore.lkml")
-        # lkml.dump may return None, in which case write an empty file
-        path.write_text(FILE_HEADER + (lkml.dump(file_lookml) or ""))
-        yield path
+) -> Path:
+    logging.info(f"Generating lookml for explore {explore_name} in {namespace}")
+    explore = EXPLORE_TYPES[explore["type"]].from_dict(explore_name, explore, views_dir)
+    file_lookml = {
+        # Looker validates all included files,
+        # so if we're not explicit about files here, validation takes
+        # forever as looker re-validates all views for every explore (if we used *).
+        "includes": [
+            f"/looker-hub/{namespace}/views/{view}.view.lkml"
+            for view in explore.get_dependent_views()
+        ],
+        "explores": explore.to_lookml(client, v1_name),
+    }
+    path = out_dir / (explore_name + ".explore.lkml")
+    # lkml.dump may return None, in which case write an empty file
+    path.write_text(FILE_HEADER + (lkml.dump(file_lookml) or ""))
+    return path
 
 
-def _generate_dashboards(
+def _generate_dashboard(
     client,
     dash_dir: Path,
     namespace: str,
-    dashboards: dict,
+    dashboard_name: str,
+    dashboard: Any,
 ):
-    for dashboard_name, dashboard_info in dashboards.items():
-        logging.info(f"Generating lookml for dashboard {dashboard_name} in {namespace}")
-        dashboard = DASHBOARD_TYPES[dashboard_info["type"]].from_dict(
-            namespace, dashboard_name, dashboard_info
-        )
+    logging.info(f"Generating lookml for dashboard {dashboard_name} in {namespace}")
+    dashboard = DASHBOARD_TYPES[dashboard["type"]].from_dict(
+        namespace, dashboard_name, dashboard
+    )
 
-        dashboard_lookml = dashboard.to_lookml(client)
-        dash_path = dash_dir / f"{dashboard_name}.dashboard.lookml"
-        dash_path.write_text(FILE_HEADER + dashboard_lookml)
-        yield dash_path
+    dashboard_lookml = dashboard.to_lookml(client)
+    dash_path = dash_dir / f"{dashboard_name}.dashboard.lookml"
+    dash_path.write_text(FILE_HEADER + dashboard_lookml)
+    return dash_path
 
 
 def _get_views_from_dict(views: Dict[str, ViewDict], namespace: str) -> Iterable[View]:
@@ -100,7 +101,9 @@ def _glean_apps_to_v1_map(glean_apps):
     return {d["name"]: d["v1_name"] for d in glean_apps}
 
 
-def _lookml(namespaces, glean_apps, target_dir, namespace_filter=[]):
+def _lookml(
+    namespaces, glean_apps, target_dir, namespace_filter=[], parallelism: int = 8
+):
     client = bigquery.Client()
 
     namespaces_content = namespaces.read()
@@ -113,6 +116,7 @@ def _lookml(namespaces, glean_apps, target_dir, namespace_filter=[]):
     with open(target / "namespaces.yaml", "w") as target_namespaces_file:
         target_namespaces_file.write(namespaces_content)
 
+    views_with_v1_name = []
     v1_mapping = _glean_apps_to_v1_map(glean_apps)
     for namespace, lookml_objects in _namespaces.items():
         if len(namespace_filter) == 0 or namespace in namespace_filter:
@@ -126,29 +130,48 @@ def _lookml(namespaces, glean_apps, target_dir, namespace_filter=[]):
 
             logging.info("  Generating views")
             v1_name: Optional[str] = v1_mapping.get(namespace)
-            for view_path in _generate_views(client, view_dir, views, v1_name):
-                logging.info(f"    ...Generating {view_path}")
+            for view in views:
+                views_with_v1_name.append((view_dir, view, v1_name))
 
+    with ThreadPool(parallelism) as pool:
+        pool.starmap(partial(_generate_view, client), views_with_v1_name)
+
+    explores_with_v1_name = []
+    for namespace, lookml_objects in _namespaces.items():
+        if len(namespace_filter) == 0 or namespace in namespace_filter:
             logging.info("  Generating datagroups")
             generate_datagroups(views, target, namespace, client)
 
+            view_dir = target / namespace / "views"
             explore_dir = target / namespace / "explores"
             explore_dir.mkdir(parents=True, exist_ok=True)
             explores = lookml_objects.get("explores", {})
             logging.info("  Generating explores")
-            for explore_path in _generate_explores(
-                client, explore_dir, namespace, explores, view_dir, v1_name
-            ):
-                logging.info(f"    ...Generating {explore_path}")
+            explores_with_v1_name += [
+                (explore_dir, namespace, explore_name, explore, view_dir, v1_name)
+                for explore_name, explore in explores.items()
+            ]
 
+    with ThreadPool(parallelism) as pool:
+        pool.starmap(partial(_generate_explore, client), explores_with_v1_name)
+
+    dashboards_with_namespace = []
+    for namespace, lookml_objects in _namespaces.items():
+        if len(namespace_filter) == 0 or namespace in namespace_filter:
             logging.info("  Generating dashboards")
             dashboard_dir = target / namespace / "dashboards"
             dashboard_dir.mkdir(parents=True, exist_ok=True)
             dashboards = lookml_objects.get("dashboards", {})
-            for dashboard_path in _generate_dashboards(
-                client, dashboard_dir, namespace, dashboards
-            ):
-                logging.info(f"    ...Generating {dashboard_path}")
+            dashboards_with_namespace += [
+                (dashboard_dir, namespace, dashboard_name, dashboard)
+                for dashboard_name, dashboard in dashboards.items()
+            ]
+
+    with ThreadPool(parallelism) as pool:
+        pool.starmap(
+            partial(_generate_dashboard, client),
+            dashboards_with_namespace,
+        )
 
 
 @click.command(help=__doc__)
@@ -182,10 +205,19 @@ def _lookml(namespaces, glean_apps, target_dir, namespace_filter=[]):
     default=[],
     help="List of namespace names to generate lookml for.",
 )
-def lookml(namespaces, app_listings_uri, target_dir, metric_hub_repos, only):
+@click.option(
+    "--parallelism",
+    "-p",
+    default=8,
+    type=int,
+    help="Number of threads to use for lookml generation",
+)
+def lookml(
+    namespaces, app_listings_uri, target_dir, metric_hub_repos, only, parallelism
+):
     """Generate lookml from namespaces."""
     if metric_hub_repos:
         MetricsConfigLoader.update_repos(metric_hub_repos)
 
     glean_apps = _get_glean_apps(app_listings_uri)
-    return _lookml(namespaces, glean_apps, target_dir, only)
+    return _lookml(namespaces, glean_apps, target_dir, only, parallelism)
