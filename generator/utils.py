794c884ca41829ce7eac3ce8a186b56cbd87026e
Anna Scholtz <anna@scholtzan.net>
2025-10-24 10:12:33 -0700

    [DENG-8802] Migrate to Github Actions (#1325)
    * [DENG-8802] Migrate to Github Actions
    
    * Remove CircleCI
    
    * Fetch ID token from env
    
    * Fix lint
    
    * Remove integration test
    
    * Force ipv4
    
    * Clone looker-hub
    
    * test dryrun error
    
    * Dryrun retry
    
    * Remove deleted events_daily view
    
    * Fix path
    
    * Remove logging
    
    ---------
    
    Co-authored-by: akkomar <akkomar@users.noreply.github.com>

--- generator/utils.py
+++ generator/utils.py
@@ -1,9 +1,12 @@
 """Utils."""
 
+import os
 import urllib.request
 from pathlib import Path
 
 LOOKER_HUB_URL = "https://raw.githubusercontent.com/mozilla/looker-hub/main"
+
+LOOKER_HUB_DIR = os.getenv("LOOKER_HUB_DIR")
 
 
 def get_file_from_looker_hub(path: Path):
@@ -11,10 +14,16 @@ def get_file_from_looker_hub(path: Path):
     file = path.name
     artifact_type = path.parent.name
     namespace = path.parent.parent.name
-    print(f"{LOOKER_HUB_URL}/{namespace}/{artifact_type}/{file}")
-    with urllib.request.urlopen(
-        f"{LOOKER_HUB_URL}/{namespace}/{artifact_type}/{file}"
-    ) as response:
-        lookml = response.read().decode(response.headers.get_content_charset())
+    if LOOKER_HUB_DIR:
+        src = Path(LOOKER_HUB_DIR) / namespace / artifact_type / file
+        print(f"local: {src}")
         path.parent.mkdir(parents=True, exist_ok=True)
-        path.write_text(lookml)
+        path.write_text(src.read_text(encoding="utf-8"), encoding="utf-8")
+    else:
+        print(f"{LOOKER_HUB_URL}/{namespace}/{artifact_type}/{file}")
+        with urllib.request.urlopen(
+            f"{LOOKER_HUB_URL}/{namespace}/{artifact_type}/{file}"
+        ) as response:
+            lookml = response.read().decode(response.headers.get_content_charset())
+            path.parent.mkdir(parents=True, exist_ok=True)
+            path.write_text(lookml)
